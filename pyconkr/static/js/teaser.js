e456fd1bfb164d086c2943d7e331f0da503ac59c
aeyeong <iiogmgo@ridi.com>
2018-04-09 23:00:17 +0900

    마크업 진행 2차 : 모바일 대응, 오브젝트 위치(일부 진행중)

--- pyconkr/static/js/teaser.js
+++ pyconkr/static/js/teaser.js
@@ -1,5 +1,10 @@
-
 $(document).ready(function(){
-  var config = {dots: true, prevArrow: false, nextArrow: false};
+  var config = {
+    dots: true,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 5000
+  };
   $('.js_slick').slick(config);
 });
