c76677eb3c930e94bb661e8b429122ff3b3366a2
aeyeong <iiogmgo@ridi.com>
2018-04-12 11:56:03 +0900

    네이버 지도 js 적용 / 모바일 대응

--- pyconkr/static/js/teaser.js
+++ pyconkr/static/js/teaser.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
-  var config = {
+  var mapOptions = {
+    center: new naver.maps.LatLng(37.5117731, 127.0592041),
+    zoom: 10,
+    zoomControl: true
+  };
+
+  var map = new naver.maps.Map('map', mapOptions);
+
+  var marker = new naver.maps.Marker({
+    position: new naver.maps.LatLng(37.5117731, 127.0592041),
+    map: map
+  });
+
+  var Slickconfig = {
     dots: true,
     prevArrow: false,
     nextArrow: false,
     autoplay: true,
     autoplaySpeed: 5000
   };
-  $('.js_slick').slick(config);
+
+  $('.js_slick').slick(Slickconfig);
 });
