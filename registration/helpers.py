07d2d3ca0ffaf72b7b1bb854a4b0113d4eaef03c
Soyoun(Rose) Kim <makeajourney@gmail.com>
2018-05-19 17:53:46 +0900

    is_registration_open() helper function 삭제

--- registration/helpers.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from constance import config
-
-
-def is_registration_open():
-    open_datetime = datetime.combine(config.REGISTRATION_OPEN, config.REGISTRATION_OPEN_TIME)
-    close_datetime = datetime.combine(config.REGISTRATION_CLOSE, config.REGISTRATION_CLOSE_TIME)
-    return open_datetime <= datetime.now() <= close_datetime
