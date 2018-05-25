713b0d632bdae7eb99edaae174b1e9e230669f76
Noh Seho <iam@sehonoh.kr>
2018-05-25 21:11:51 +0900

    https://github.com/pythonkr/pyconkr-2018/issues/23 이슈 작업

--- registration/admin.py
+++ registration/admin.py
@@ -1,85 +1,111 @@
 # -*- coding: utf-8 -*-
+from constance import config
 from django.contrib import admin
 from django.core.mail import send_mass_mail
 from django.shortcuts import render
-from constance import config
 from django.utils import timezone
-from .iamporter import get_access_token, Iamporter, IamporterError
+from iamport import Iamport
 
 from .models import Registration, Option, ManualPayment, IssueTicket
 
 
-def send_bankpayment_alert_email(modeladmin, request, queryset):
+def send_email_about_pending_vbank_transfer(modeladmin, request, queryset):
     messages = []
-    subject = u"PyCon Korea 2017 입금확인부탁드립니다. Please Check PyCon Korea 2017 payment"
-    body = u"""
-    안녕하세요. PyCon Korea 준비위원회입니다.
-    현재 입금여부를 확인하였으나 입금이 되지 않았습니다.
-    혹시나 다른 이름으로 입금하신분은 support@pycon.kr 로 메일 부탁드립니다.
-    입금시한은 구매로부터 일주일입니다.
-    감사합니다.
-    """
-    from_email = "pyconkr@pycon.kr"
+    subject = '파이콘 한국 2018 가상 계좌 입금 대기 안내, Please Check your virtual bank transfer for PyCon Korea 2018 Ticket'
+    body = '''
+안녕하세요. 파이콘 한국 2018 준비위원회입니다.
+
+파이콘 한국 2018 신청의 결제 방법으로 가상 계좌를 선택해주셨으나,
+입금 여부가 확인되지 않고 있습니다.
+
+혹시 다른 이름으로 입금하신 분은 support@pycon.kr로 메일 남겨주시기 바랍니다.
+
+입금 마감 기간은 구매일로부터 일주일 입니다.
+감사합니다.
+
+Hi, This is PyCon Korea 2018 Organizing Team.
+
+You choose the virtual bank transfer for PyCon Korea 2018 Registration,
+But we still cannot check your transfer history.
+
+If you already transferred, please send an e-mail to support@pycon.kr.
+If you did not, you can transfer by a week from you registered date.
+Thank you.
+
+Best Regards.
+    '''
+    from_email = 'pyconkr@pycon.kr'
     for obj in queryset:
         email = obj.email
         message = (subject, body, from_email, [email])
         messages.append(message)
     send_mass_mail(messages, fail_silently=False)
 
-send_bankpayment_alert_email.short_description = "Send Bank Payment Email"
+
+send_email_about_pending_vbank_transfer.short_description = 'Send Virtual Bank Transfer Email'
 
 
 def cancel_registration(modeladmin, request, queryset):
     messages = []
-    subject = u"PyCon Korea 2017 결제 취소 알림"
-    body = u"""
-안녕하세요. PyCon Korea 준비위원회입니다.
+    subject = '파이콘 한국 2018 결제 취소 알림, PyCon Korea 2018 Ticket is cancelled successfully'
+    body = '''
+안녕하세요. 파이콘 한국 2018 준비위원회입니다.
 
 결제가 취소되었음을 알려드립니다.
-결제 대행사 사정에 따라 다소 늦게 카드 취소가 이뤄질 수 있습니다.
+결제 대행사 사정에 따라 다소 늦게 결제 취소가 이뤄질 수 있습니다.
 
 다른 문의 사항은 support@pycon.kr 로 메일 부탁드립니다.
 감사합니다.
-    """
-    from_email = "pyconkr@pycon.kr"
+
+Hi, This is PyCon Korea 2018 Organizing Team.
+
+We are sending this email for your PyCon Korea 2018 Ticket is cancelled successfully.
+Because of Payment Gateway's reasons, It will be delayed.
+
+If you have a question, feel free to e-mail to support@pycon.kr.
+Thank you.
+
+Best Regards.
+    '''
+    from_email = 'pyconkr@pycon.kr'
 
     results = []
     now = timezone.now()
-    access_token = get_access_token(config.IMP_API_KEY, config.IMP_API_SECRET)
-    imp_client = Iamporter(access_token)
 
     for obj in queryset:
         if obj.payment_method != 'card':
-            obj.cancel_reason = u'카드 결제만 취소 가능'
+            obj.cancel_reason = '카드 결제만 취소 가능'
             results.append(obj)
             continue
 
         if obj.payment_status != 'paid':
-            obj.cancel_reason = u'결제 완료만 취소 가능'
+            obj.cancel_reason = '결제 완료만 취소 가능'
             results.append(obj)
             continue
 
         if not obj.option.is_cancelable:
-            obj.cancel_reason = u'취소 불가능 옵션 (얼리버드 등)'
+            obj.cancel_reason = '취소 불가능 옵션 (얼리버드 등)'
             results.append(obj)
             continue
 
         if obj.option.cancelable_date and obj.option.cancelable_date < now:
-            obj.cancel_reason = u'취소가능일자가 아니므로 취소 불가능'
+            obj.cancel_reason = '취소가능일자가 아니므로 취소 불가능'
             results.append(obj)
             continue
 
         try:
-            imp_params = dict(
-                merchant_uid=obj.merchant_uid,
-                reason=u'Cancel by admin',
-            )
-            imp_client.cancel(**imp_params)
+            iamport = Iamport(imp_key=config.IMP_API_KEY, imp_secret=config.IMP_API_SECRET)
+            iamport.cancel_by_merchant_uid('Django Admin에서 결제 취소', obj.merchant_uid)
         except IOError:
             obj.cancel_status = 'IOError'
             results.append(obj)
             continue
-        except IamporterError as e:
+        except Iamport.HttpError as e:
+            obj.cancel_status = e.code
+            obj.cancel_reason = e.reason
+            results.append(obj)
+            continue
+        except Iamport.ResponseError as e:
             obj.cancel_status = e.code
             obj.cancel_reason = e.message
             results.append(obj)
@@ -98,42 +124,44 @@ def cancel_registration(modeladmin, request, queryset):
     send_mass_mail(messages, fail_silently=False)
     return render(request, 'registration/cancellation_result.html', {'results': results})
 
-cancel_registration.short_description = "Cancel registration"
+
+cancel_registration.short_description = 'Cancel registration'
 
 
+@admin.register(Option)
 class OptionAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_active',
                     'begin_at', 'closed_at',
-                    'price', 'is_cancelable', 'cancelable_date', )
-    list_editable = ('is_active', )
-    ordering = ('id', )
-admin.site.register(Option, OptionAdmin)
+                    'price', 'is_cancelable', 'cancelable_date',)
+    list_editable = ('is_active',)
+    ordering = ('id',)
 
 
+@admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
     list_display = ('user', 'option', 'name', 'email', 'payment_method',
                     'payment_status', 'created', 'confirmed', 'canceled')
     list_editable = ('payment_status',)
     list_filter = ('option', 'payment_method', 'payment_status')
     csv_fields = ['name', 'email', 'company', 'option', ]
-    search_fields = ('name', 'email', 'merchant_uid', 'transaction_code', )
-    readonly_fields = ('created', 'merchant_uid', 'transaction_code', )
+    search_fields = ('name', 'email', 'merchant_uid', 'transaction_code',)
+    readonly_fields = ('created', 'merchant_uid', 'transaction_code',)
     ordering = ('id',)
-    actions = (send_bankpayment_alert_email, cancel_registration)
-admin.site.register(Registration, RegistrationAdmin)
+    actions = (send_email_about_pending_vbank_transfer, cancel_registration)
 
 
+@admin.register(IssueTicket)
 class IssueTicketAdmin(admin.ModelAdmin):
     list_display = ('registration', 'issuer', 'issue_date')
     ordering = ('issue_date',)
-admin.site.register(IssueTicket, IssueTicketAdmin)
 
+
+@admin.register(ManualPayment)
 class ManualPaymentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'payment_status', 'user', )
-    list_filter = ('payment_status', )
-    search_fields = ('user__email', 'description', )
-    raw_id_fields = ('user', )
+    list_display = ('title', 'payment_status', 'user',)
+    list_filter = ('payment_status',)
+    search_fields = ('user__email', 'description',)
+    raw_id_fields = ('user',)
 
     class Meta:
         model = ManualPayment
-admin.site.register(ManualPayment, ManualPaymentAdmin)
--- registration/views.py
+++ registration/views.py
@@ -13,6 +13,7 @@ from django.utils.translation import ugettext as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.views.generic import DetailView
+from iamport import Iamport
 
 from pyconkr.helper import render_io_error
 from .forms import (RegistrationForm, RegistrationAdditionalPriceForm,
@@ -26,11 +27,13 @@ payment_logger = logging.getLogger('payment')
 
 def index(request):
     is_registered = False
+
     if request.user.is_authenticated():
         is_registered = (Registration.objects
                          .filter(user=request.user,
                                  payment_status__in=['paid', 'ready'])
                          .exists())
+
     options = Option.objects.active_conference()
     ctx = {
         'options': options,
@@ -42,8 +45,10 @@ def index(request):
 @login_required
 def status(request):
     registration = Registration.objects.filter(user=request.user)
+
     if registration:
         registration = registration.latest('created')
+
     context = {
         'registration': registration,
         'title': _("Registration Status"),
@@ -54,6 +59,7 @@ def status(request):
 @login_required
 def payment(request, option_id):
     product = Option.objects.get(id=option_id)
+
     if not product.is_opened:
         return redirect('registration_index')
 
@@ -66,6 +72,7 @@ def payment(request, option_id):
         return redirect('registration_status')
 
     uid = str(uuid4()).replace('-', '')
+
     if product.has_additional_price:
         form = RegistrationAdditionalPriceForm(initial={'email': request.user.email,
                                                         'option': product,
@@ -74,22 +81,23 @@ def payment(request, option_id):
         form = RegistrationForm(initial={'email': request.user.email,
                                          'option': product,
                                          'base_price': product.price})
+
     return render(request, 'registration/payment.html', {
         'title': _('Registration'),
         'form': form,
         'uid': uid,
         'product_name': product.name,
         'amount': product.price,
-        'vat': 0,
     })
 
 
 @login_required
 def payment_process(request):
+    # 비정상적 접근으로 등록 메인 페이지로 이동합니다.
     if request.method == 'GET':
         return redirect('registration_index')
 
-    # alreay registered
+    # 이미 등록된 사용자로 등록 현황 페이지로 이동합니다.
     if Registration.objects.filter(user=request.user, payment_status__in=['paid', 'ready']).exists():
         return redirect('registration_status')
 
@@ -102,76 +110,79 @@ def payment_process(request):
         form_errors_string = "\n".join(('%s:%s' % (k, v[0]) for k, v in form.errors.items()))
         return JsonResponse({
             'success': False,
-            'message': form_errors_string,  # TODO : ...
+            'message': form_errors_string,
         })
 
-    remain_ticket_count = (config.TOTAL_TICKET - Registration.objects.filter(payment_status__in=['paid', 'ready']).count())
+    remain_ticket_count = (
+            config.TOTAL_TICKET - Registration.objects.filter(payment_status__in=['paid', 'ready']).count())
 
-    # sold out
+    # 매진 상태
     if remain_ticket_count <= 0:
         return JsonResponse({
             'success': False,
-            'message': u'티켓이 매진 되었습니다',
+            'message': '티켓이 매진 되었습니다',
         })
 
+    # 후원 추가 금액이 0원 미만일 때
     if form.cleaned_data.get('additional_price', 0) < 0:
         return JsonResponse({
             'success': False,
             'message': u'후원 금액은 0원 이상이어야 합니다.',
         })
 
-    registration = Registration(
-            user=request.user,
-            name = form.cleaned_data.get('name'),
-            email = request.user.email,
-            additional_price = form.cleaned_data.get('additional_price', 0),
-            company = form.cleaned_data.get('company', ''),
-            top_size = form.cleaned_data.get('top_size', ''),
-            phone_number = form.cleaned_data.get('phone_number', ''),
-            merchant_uid = request.POST.get('merchant_uid'),
-            option = form.cleaned_data.get('option'),
-            payment_method = form.cleaned_data.get('payment_method')
-        )
+    cleaned_form = form.cleaned_data
 
-    # sold out
-    if registration.option.is_soldout:
+    registration = Registration(
+        user=request.user,
+        email=request.user.email,
+        merchant_uid=request.POST.get('merchant_uid'),
+        name=cleaned_form.get('name'),
+        additional_price=cleaned_form.get('additional_price', 0),
+        company=cleaned_form.get('company', ''),
+        top_size=cleaned_form.get('top_size', ''),
+        phone_number=cleaned_form.get('phone_number', ''),
+        option=cleaned_form.get('option'),
+        payment_method=cleaned_form.get('payment_method')
+    )
+
+    # 다시 한 번 매진 확인
+    if registration.option.is_sold_out:
         return JsonResponse({
             'success': False,
-            'message': u'{name} 티켓이 매진 되었습니다'.format(name=registration.option.name),
+            'message': '{name} 티켓이 매진 되었습니다'.format(name=registration.option.name),
         })
 
     try:
         product = registration.option
+        post_data = request.POST
 
-        if registration.payment_method == 'card':
-            access_token = get_access_token(config.IMP_API_KEY, config.IMP_API_SECRET)
-            imp_client = Iamporter(access_token)
+        # 한국인용 카드 결제일 때
+        if registration.payment_method == 'card-korean':
+            iamport = Iamport(config.IMP_DOM_API_KEY, config.IMP_DOM_API_SECRET)
+
             # TODO : use validated and cleaned data
-            imp_params = dict(
-                token=request.POST.get('token'),
-                merchant_uid=request.POST.get('merchant_uid'),
-                amount=product.price + registration.additional_price,
-                card_number=request.POST.get('card_number'),
-                expiry=request.POST.get('expiry'),
-                birth=request.POST.get('birth'),
-                pwd_2digit=request.POST.get('pwd_2digit'),
-                customer_uid=form.cleaned_data.get('email'),
+            iamport_params = dict(
                 name=product.name,
-                buyer_name=request.POST.get('name'),
-                buyer_email=request.POST.get('email'),
-                buyer_tel=request.POST.get('phone_number')
-
+                amount=product.price + registration.additional_price,
+                token=post_data.get('token'),
+                merchant_uid=post_data.get('merchant_uid'),
+                card_number=post_data.get('card_number'),
+                expiry=post_data.get('expiry'),
+                birth=post_data.get('birth'),
+                pwd_2digit=post_data.get('pwd_2digit'),
+                vat=0,
+                buyer_name=post_data.get('name'),
+                buyer_email=post_data.get('email'),
+                buyer_tel=post_data.get('phone_number')
             )
-            if request.POST.get('birth') == '':
-                # foreign payment
-                imp_client.foreign(**imp_params)
-            else:
-                imp_client.foreign(**imp_params)
-                # imp_client.onetime(**imp_params)
-            confirm = imp_client.find_by_merchant_uid(request.POST.get('merchant_uid'))
 
+            iamport.pay_onetime(**iamport_params)
+
+            confirm = iamport.find_by_merchant_uid(post_data.get('merchant_uid'))
+
+            # 결제한 금액과 Form에 입력된 결제 금액들이 다를 때
             if confirm['amount'] != product.price + registration.additional_price:
-                # TODO : cancel
+                # TODO : 결제 취소하는 로직 있어야 함
                 return render_io_error("amount is not same as product.price. it will be canceled")
 
             registration.transaction_code = confirm.get('pg_tid')
@@ -183,16 +194,55 @@ def payment_process(request):
             registration.vbank_date = confirm.get('vbank_date', None)
             registration.vbank_holder = confirm.get('vbank_holder', None)
             registration.save()
-        elif registration.payment_method == 'vbank':
-            registration.transaction_code = request.POST.get('pg_tid')
-            registration.payment_method = request.POST.get('pay_method')
-            registration.payment_status = request.POST.get('status')
-            registration.payment_message = request.POST.get('fail_reason')
-            registration.vbank_name = request.POST.get('vbank_name', None)
-            registration.vbank_num = request.POST.get('vbank_num', None)
-            registration.vbank_date = request.POST.get('vbank_date', None)
-            registration.vbank_holder = request.POST.get('vbank_holder', None)
+
+        elif registration.payment_method == 'card-foreign':
+            iamport = Iamport(config.IMP_INTL_API_KEY, config.IMP_INTL_API_SECRET)
+
+            # TODO : use validated and cleaned data
+            iamport_params = dict(
+                name=product.name,
+                amount=product.price + registration.additional_price,
+                token=post_data.get('token'),
+                merchant_uid=post_data.get('merchant_uid'),
+                card_number=post_data.get('card_number'),
+                expiry=post_data.get('expiry'),
+                birth=post_data.get('birth'),
+                pwd_2digit=post_data.get('pwd_2digit'),
+                vat=0,
+                buyer_name=post_data.get('name'),
+                buyer_email=post_data.get('email'),
+                buyer_tel=post_data.get('phone_number')
+            )
+            iamport.pay_foreign(**iamport_params)
+
+            confirm = iamport.find_by_merchant_uid(post_data.get('merchant_uid'))
+
+            # 결제한 금액과 Form에 입력된 결제 금액들이 다를 때
+            if confirm['amount'] != product.price + registration.additional_price:
+                # TODO : 결제 취소하는 로직 있어야 함
+                return render_io_error("amount is not same as product.price. it will be canceled")
+
+            registration.transaction_code = confirm.get('pg_tid')
+            registration.payment_method = confirm.get('pay_method')
+            registration.payment_status = confirm.get('status')
+            registration.payment_message = confirm.get('fail_reason')
+            registration.vbank_name = confirm.get('vbank_name', None)
+            registration.vbank_num = confirm.get('vbank_num', None)
+            registration.vbank_date = confirm.get('vbank_date', None)
+            registration.vbank_holder = confirm.get('vbank_holder', None)
             registration.save()
+
+        elif registration.payment_method == 'vbank':
+            registration.transaction_code = post_data.get('pg_tid')
+            registration.payment_method = post_data.get('pay_method')
+            registration.payment_status = post_data.get('status')
+            registration.payment_message = post_data.get('fail_reason')
+            registration.vbank_name = post_data.get('vbank_name', None)
+            registration.vbank_num = post_data.get('vbank_num', None)
+            registration.vbank_date = post_data.get('vbank_date', None)
+            registration.vbank_holder = post_data.get('vbank_holder', None)
+            registration.save()
+
         else:
             raise Exception('Unknown payment method')
 
@@ -213,19 +263,39 @@ def payment_process(request):
 def payment_callback(request):
     merchant_uid = request.POST.get('merchant_uid')
     registration = Registration.objects.filter(merchant_uid=merchant_uid)
+
     if not registration.exists():
         return HttpResponse(status_code=404)
-    access_token = get_access_token(config.IMP_API_KEY, config.IMP_API_SECRET)
-    imp_client = Iamporter(access_token)
-    result = imp_client.find_by_merchant_uid(merchant_uid)
-    registration = registration.first()
-    if result['status'] == 'paid':
-        registration.confirmed = datetime.datetime.now()
-    elif result['status'] == 'cancelled':
-        registration.canceled = datetime.datetime.now()
-    registration.payment_status = result['status']
-    registration.save()
-    return HttpResponse()
+
+    try:
+        iamport = Iamport(config.IMP_DOM_API_KEY, config.IMP_DOM_API_SECRET)
+        result = iamport.find_by_merchant_uid(merchant_uid)
+        registration = registration.first()
+
+        if result['status'] == 'paid':
+            registration.confirmed = datetime.datetime.now()
+        elif result['status'] == 'cancelled':
+            registration.canceled = datetime.datetime.now()
+
+        registration.payment_status = result['status']
+        registration.save()
+        return HttpResponse()
+    except Iamport.ResponseError as iamport_error:
+        if iamport_error.code == 401 or iamport_error.code == 404:
+            iamport = Iamport(config.IMP_INTL_API_KEY, config.IMP_INTL_API_SECRET)
+            result = iamport.find_by_merchant_uid(merchant_uid)
+            registration = registration.first()
+
+            if result['status'] == 'paid':
+                registration.confirmed = datetime.datetime.now()
+            elif result['status'] == 'cancelled':
+                registration.canceled = datetime.datetime.now()
+
+            registration.payment_status = result['status']
+            registration.save()
+            return HttpResponse()
+
+    return HttpResponse(status_code=404)
 
 
 @login_required
@@ -245,7 +315,6 @@ def manual_registration(request, manual_payment_id):
         'uid': uid,
         'product_name': mp.title,
         'amount': mp.price,
-        'vat': 0,
         'payment_status': mp.payment_status,
     })
 
@@ -293,7 +362,6 @@ def manual_payment_process(request):
             expiry=request.POST.get('expiry'),
             birth=request.POST.get('birth'),
             pwd_2digit=request.POST.get('pwd_2digit'),
-            customer_uid=form.cleaned_data.get('email'),
             name=mp.title,
             buyer_name=request.POST.get('name', ''),
             buyer_email=request.POST.get('email'),
@@ -342,6 +410,7 @@ def group_required(*group_names):
             if bool(u.groups.filter(name__in=group_names)) or u.is_superuser:
                 return True
         return False
+
     return user_passes_test(in_groups)
 
 
@@ -354,14 +423,15 @@ def issue(request):
     }
     return render(request, 'registration/issue_ticket.html', context)
 
+
 @group_required('admin', 'organizer', 'volunteer')
 def issue_print(request, registration_id):
     registration = get_object_or_404(Registration, id=registration_id)
-    name = registration.user.profile.name if registration.user.profile.name != ''\
-            else registration.name
+    name = registration.user.profile.name if registration.user.profile.name != '' \
+        else registration.name
     company = registration.user.profile.organization if \
-            registration.user.profile.organization != '' else registration.company
-    company = '' if company == None else company
+        registration.user.profile.organization != '' else registration.company
+    company = '' if company is None else company
     context = {
         'name': name,
         'company': company,
@@ -370,16 +440,19 @@ def issue_print(request, registration_id):
     }
     return render(request, 'registration/issue_print.html', context)
 
+
 @group_required('admin', 'organizer', 'volunteer')
 @require_http_methods(["POST"])
 def issue_submit(request):
     user_data = IssueSubmitForm(request.POST)
+
     if not user_data.is_valid():
         return HttpResponseBadRequest('invalid form value')
+
     user_id = user_data.cleaned_data['user_id']
     registration = Registration.objects.get(payment_status='paid',
                                             id=user_id)
-    issue=IssueTicket(
+    issue = IssueTicket(
         registration=registration,
         issuer=request.user
     ).save()
