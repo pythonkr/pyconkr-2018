c873461644a598cb9f1f4500ee5e9fdc9cf70037
Soyoun(Rose) Kim <makeajourney@gmail.com>
2018-06-02 23:22:32 +0900

    base에서 footer.html include하도록 변경. i18n 관련 message 2017 -> 2018로 변경

--- pyconkr/context_processors.py
+++ pyconkr/context_processors.py
@@ -23,9 +23,9 @@ def default(request):
             'title': _('About'),
             'icon': 'python',
             'submenu': OrderedDict([
-                ('pyconkr', {'title': _('About PyCon Korea 2017')}),
+                ('pyconkr', {'title': _('About PyCon Korea 2018')}),
                 ('coc', {'title': _('Code of Conduct')}),
-                ('blog', {'title': _('PyCon.KR 2017 Blog')}),
+                ('blog', {'title': _('PyCon Korea Blog')}),
                 ('announcements', {'title': _('Announcements')}),
                 ('sponsor', {'title': _('Sponsors')}),
                 ('patron', {'title': _('Patrons')}),
